612daca5ebe48fedc85b5377e432eca53849df2c
c5w227 <42083103+c5w227@users.noreply.github.com>
2019-07-28 11:05:08 +0530

    Update settings.py

--- src/trydjango/settings.py
+++ src/trydjango/settings.py
@@ -75,35 +75,26 @@ WSGI_APPLICATION = 'trydjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'pricetrack',
-#         'USER':'root',
-#         'PASSWORD':'',
-#         'HOST':'localhost',
-#         'PORT':'3306',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+
 # AWS Configuration
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'jayd',
-#         'USER':'jayd',
-#         'PASSWORD':'jaydhanani333666',
-#         'HOST':'pricetracker.c9pn9fmsleba.us-east-2.rds.amazonaws.com',
-#         'PORT':'3306',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'jayd',
+        'USER':'jayd',
+        'PASSWORD':'jaydhanani333666',
+        'HOST':'pricetracker.c9pn9fmsleba.us-east-2.rds.amazonaws.com',
+        'PORT':'3306',
+    }
+}
 
 # $servername = "localhost";
 # $username = "id6751081_smaletric_user";
